a49cf426e1ecef09cc944aa1cc0f7988405d5777
JimmitoDevs <jimmycabalz.a.2011@gmail.com>
2025-04-13 18:50:22 -0600

    Agregar archivos de proyecto.

---
+++ TeslaAppMovilFinal2.0/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TeslaAppMovilFinal2._0
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
