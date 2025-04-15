7fe11c5bfd00bb6e8aecb0f6c396854544162b43
segniinii <dsegnini04@gmail.com>
2025-04-14 22:43:55 -0600

    Avance 1
    Landing Page
    Estilo
    Login

---
+++ TeslaAppMovilFinal2.0/Models/Vehiculo.cs
@@ -0,0 +1,12 @@
+﻿namespace TeslaAppMovilFinal2._0.Models
+{
+    public class Vehiculo
+    {
+        public string Modelo { get; set; }
+        public int Precio { get; set; }
+        public string ImagenUrl { get; set; }
+        public string Autonomia { get; set; }
+        public string Aceleracion { get; set; }
+        public string Velocidad { get; set; }
+    }
+}
