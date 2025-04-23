daffdba70fca1acb44637adaf5910b47f899be00
JimmitoDevs <jimmycabalz.a.2011@gmail.com>
2025-04-22 18:04:09 -0600

    vehiculo clases

--- TeslaAppMovilFinal2.0/Models/Vehiculo.cs
+++ TeslaAppMovilFinal2.0/Models/Vehiculo.cs
@@ -8,5 +8,10 @@
         public string Autonomia { get; set; }
         public string Aceleracion { get; set; }
         public string Velocidad { get; set; }
+        public string Codigo { get; set; }
+        public string Marca { get; set; }
+        public int Año { get; set; }
+        public double Kilometraje { get; set; }
+        public string Estado { get; set; } // Nuevo o Usado
     }
 }
